bcfd6c4416398e87227bb4d4a4b344d166aaa9b8
iPrq <danciju@gmail.com>
2025-01-07 23:31:22 +0530

    Added functionality to function keys

--- script.js
+++ script.js
@@ -1,18 +1,96 @@
 const inputdisplay = document.getElementById("inputdisplay");
+const outputdisplay = document.getElementById("outputdisplay");
 let addpressed = false;
+let number1, functiontype;
 function onInputKey(input) {
-    inputdisplay.value += input;
+        if(outputdisplay.value !== "") {
+            inputdisplay.value = "";
+            outputdisplay.value = "";
+            addpressed = false;
+            inputdisplay.value += input;
+        }
+        else {
+            inputdisplay.value += input;
+        }
 }
 
 function onClearKey() {
     inputdisplay.value = ``;
+    outputdisplay.value = "";
+    addpressed = false;
 }
 
 function onFunctionKey(input) {
-
-    switch(input) {
-        case `add`:
-            add(a+b)
+    if(!addpressed) {
+        switch(input) {
+            case "add":
+                number1 = inputdisplay.value;
+                inputdisplay.value += '+';
+                functiontype = "+";
+                addpressed = true;
+                break;
+            case "sub":
+                number1 = inputdisplay.value;
+                inputdisplay.value += '-';
+                functiontype = "-";
+                addpressed = true;
+                break;
+            case "mul":
+                number1 = inputdisplay.value;
+                inputdisplay.value += '×';
+                functiontype = "×";
+                addpressed = true;
+                break;
+            case "div":
+                number1 = inputdisplay.value;
+                inputdisplay.value += '÷';
+                functiontype = "÷";
+                addpressed = true;
+                break;
+            default:
+                break;
+        }
     }
+}
+function calculate() {
+    let value = inputdisplay.value;
+    let a = number1;
+    let b = value.substring(value.indexOf(functiontype)+1)
+    console.log(b);
+    if(addpressed) {
+        switch(functiontype) {
+            case "+":
+                add(a,b);
+                console.log("running");
+                break;
+            case "-":
+                sub(a,b);
+                break;
+            case "×":
+                mul(a,b);
+                break;
+            case "÷":
+                div(a,b);
+                break;
+            default:
+                console.log("Not added")
+                break;
+        }
+    }
+}
+
+function add(a,b) {
+    outputdisplay.value = Number(a)+Number(b);
+}
+
+function sub(a,b) {
+    outputdisplay.value = Number(a)-Number(b);
+}
+
+function mul(a,b){
+    outputdisplay.value = Number(a)*Number(b);
+}
+function div(a,b){
+    outputdisplay.value = Number(a)/Number(b);
 }
 
