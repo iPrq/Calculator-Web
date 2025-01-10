26fef1ff0a9d52077372613a82eccb6d44c5c6e3
iPrq <danciju@gmail.com>
2025-01-10 18:54:04 +0530

    Added functionality to history.html

--- Website/script.js
+++ Website/script.js
@@ -4,6 +4,7 @@ const outputdisplay = document.getElementById("outputdisplay");
 let addpressed = false;
 let number1, functiontype;
 let funcinput;
+let newWindow;
 function onInputKey(input) {
         if(outputdisplay.value !== "") {
             inputdisplay.value = "";
@@ -58,57 +59,77 @@ function onFunctionKey(input) {
 function calculate() {
     let value = inputdisplay.value;
     let a = number1;
+    let output;
     let b = value.substring(value.indexOf(functiontype)+1)
     if(addpressed) {
         switch(functiontype) {
             case "+":
-                add(a,b);
+                output = add(a,b);
                 console.log("running");
                 break;
             case "-":
-                sub(a,b);
+                output = sub(a,b);
                 break;
             case "×":
-                mul(a,b);
+                output = mul(a,b);
                 break;
             case "÷":
-                div(a,b);
+                output = div(a,b);
                 break;
             default:
                 break;
         }
-        postCalculation(a,b,functioninput,output);
+        postCalculation(a,b,funcinput,output);
     }
 }
 
 function add(a,b) {
     outputdisplay.value = Number(a)+Number(b);
+    return Number(a)+Number(b);
 }
 
 function sub(a,b) {
     outputdisplay.value = Number(a)-Number(b);
+    return Number(a)-Number(b);
 }
 
 function mul(a,b){
     outputdisplay.value = Number(a)*Number(b);
+    return Number(a)*Number(b);
 }
 function div(a,b){
     outputdisplay.value = Number(a)/Number(b);
+    return Number(a)/Number(b);
 }
 
 function openHistory() {
-    window.open("history.html");
-    gethistory();
+   newWindow = window.open('history.html');
+   newWindow.addEventListener('DOMContentLoaded', () =>
+    {gethistory(newWindow)});
 } 
 
 
 
-function gethistory() {
 
+
+async function gethistory(newWindow) {
+    try {
+        const response = await fetch('http://localhost:8080/calculator/get', {
+            method: 'GET'
+        });
+     data = await response.json();
+     addhistory(data, newWindow)// passing the fetched data to addhistory
+    } catch (error) {
+        console.error(error);
+    }
 }
+
 
 async function postCalculation(a,b,method,o) {
     const response = await fetch('http://localhost:8080/calculator/add', {
+        headers: {
+            'Content-type': 'application/json'
+        },
         method: 'POST',
         body: JSON.stringify({number1: a, number2: b, method: method, output: o})
     });
@@ -116,7 +137,51 @@ async function postCalculation(a,b,method,o) {
         console.log("Added Calculation");
     }
     else {
-        console.error("Failed to add response");
+        console.error(error);
     }
     
 }
+
+function addhistory(data, newWindow) {
+    console.log(data);
+    if (!Array.isArray(data)) { 
+        console.error("Expected an array but got: ", data);
+     } 
+    else {
+        data.forEach(element => { createEntry(element, newWindow); }); 
+    }
+}
+
+function createEntry(element, newWindow) {
+    let newcalculation = newWindow.document.getElementById("calculationtemplate").cloneNode(true);
+    newcalculation.id = "calculation1";
+    
+    let entrys = newcalculation.querySelectorAll("p");
+    let divnumber1 = entrys[0];
+    let method = entrys[1];
+    let divnumber2 = entrys[2];
+    let divoutput = entrys[4];
+
+    divnumber1.textContent = element.number1;
+    divnumber2.textContent = element.number2;
+    method.textContent = setmethod(element.method);
+    divoutput.textContent = element.output;
+
+    newWindow.document.body.appendChild(newcalculation);
+}
+
+function setmethod(method) {
+    switch(method) {
+        case "add":
+            return "+";
+        case "sub":
+            return "-";
+        case "mul":
+            return "×";
+        case "div":
+            return "÷";
+        default:
+            return "null";
+    }
+}
+
--- calculator/src/main/java/com/calculator/configurations/WebConfig.java
+++ calculator/src/main/java/com/calculator/configurations/WebConfig.java
@@ -1,5 +1,6 @@
 package com.calculator.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,14 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // Change this to your liking to access the backend
-                        .allowedMethods("POST","GET","DELETE","PUT");
-            }
+                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                }
         };
     }
 }
