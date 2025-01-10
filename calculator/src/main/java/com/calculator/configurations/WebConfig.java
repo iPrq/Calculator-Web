75837dc33cc64fe5eef1aefbfc095f572b5eb1e8
iPrq <danciju@gmail.com>
2025-01-10 14:50:34 +0530

    Added some Configurations

---
+++ calculator/src/main/java/com/calculator/configurations/WebConfig.java
@@ -0,0 +1,20 @@
+package com.calculator.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500") // Change this to your liking to access the backend
+                        .allowedMethods("POST","GET","DELETE","PUT");
+            }
+        };
+    }
+}
